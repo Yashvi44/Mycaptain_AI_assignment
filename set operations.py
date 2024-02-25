28c99aa3c63186fd5080a37a011c29e937245b79
Yashvi44 <161220345+Yashvi44@users.noreply.github.com>
2024-02-25 21:46:45 +0530

    Add files via upload

---
+++ set operations.py
@@ -0,0 +1,18 @@
+E={0, 2, 4, 6, 8}
+N={1, 2, 3, 4, 5}
+
+# Union
+union=E.union(N)
+print("the union is:",union)
+
+# Intersection
+intersection=E.intersection(N)
+print("the intersection is:",intersection)
+
+# Difference
+diff=E.difference(N)
+print("the difference is:",diff)
+
+# Symmetric difference
+symd=E.symmetric_difference(N)
+print("the symmetric difference is:",symd)
